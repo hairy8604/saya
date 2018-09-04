a558e5f98881106d33c5d8748530f88267ac0a21
mahathir <mahathirm@rmp.gov.my>
2018-09-04 10:06:26 +0800

    versi 1

---
+++ string.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int my_strlen(char *s)
+{
+    char * p
+
+    while (*p)
+
+    ++p;
+
+    return(p - s);
+
+}
+int main (void)
+{
+    int i;
+
+    char *s[] = {
+
+        "Git tutorial",
+        "Tutorial Point"
+    };
+    for (i = 0; i < 2; ++i)
+
+    printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
+
+    return 0;
+}
